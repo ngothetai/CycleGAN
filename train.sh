2f0e72c7b9f23e68db71a50b2ad3935a7550cba9
ngothetai <thetai2382002@gmail.com>
2025-09-05 18:15:33 +0000

    Update project configuration: modified Python version requirement to 3.11, added new dependencies including torch and torchvision, and expanded .gitignore to include additional directories and files.

---
+++ train.sh
@@ -0,0 +1,9 @@
+uv run python pytorch-CycleGAN-and-pix2pix/train.py \
+--dataroot ./datasets/histology_stain \
+--name histology_stain_cyclegan \
+--model cycle_gan \
+--batch_size 8 \
+--load_size 256 \
+--crop_size 256 \
+--n_epochs 100 \
+--use_wandb
