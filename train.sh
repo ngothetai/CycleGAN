5118583e38edc7c1a625674049066d4d39a45387
ngothetai <thetai2382002@gmail.com>
2025-09-05 19:06:59 +0000

    Update training script parameters: adjust number of epochs to 10, add lambda values for cycle consistency, and set learning rate.

--- train.sh
+++ train.sh
@@ -3,8 +3,13 @@ uv run python pytorch-CycleGAN-and-pix2pix/train.py \
 --name histology_stain_cyclegan \
 --model cycle_gan \
 --batch_size 8 \
+--lambda_A 5.0 \
+--lambda_B 5.0 \
+--lambda_identity 0.1 \
+--lr 0.0003
 --load_size 256 \
 --crop_size 256 \
---n_epochs 100 \
+--n_epochs 10 \
+--save_epoch_freq 1 \
 --use_wandb
 # --continue_train
