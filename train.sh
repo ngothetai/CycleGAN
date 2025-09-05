75b3f40931874a3848bce8fce9744fb58d23f50f
ngothetai <thetai2382002@gmail.com>
2025-09-05 19:11:17 +0000

    Rename training session in script for clarity: updated from 'histology_stain_cyclegan' to 'histology_stain_cyclegan_training'.

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 uv run python pytorch-CycleGAN-and-pix2pix/train.py \
 --dataroot ./datasets/histology_stain \
---name histology_stain_cyclegan \
+--name histology_stain_cyclegan_training \
 --model cycle_gan \
 --batch_size 8 \
 --lambda_A 5.0 \
 --lambda_B 5.0 \
 --lambda_identity 0.1 \
---lr 0.0003
+--lr 0.0003 \
 --load_size 256 \
 --crop_size 256 \
 --n_epochs 10 \
